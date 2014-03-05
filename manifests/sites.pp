44c4c8128e43c58abeae0b40a6f7830e047ea010
Eric Williams <eric@subcritical.org>
2014-03-05 16:30:36 +0000

    Updated for the django demo.

--- manifests/sites.pp
+++ manifests/sites.pp
@@ -1,7 +1,17 @@
 import  'nodes.pp'
 
 node default {
-    
+    include nginx
+}
+
+user { 'ubuntu':
+    comment => 'Ubuntu User',
+    home => '/home/ubuntu',
+    ensure => present,
+    managehome => true,
+    #shell => '/bin/bash',
+    #uid => '501',
+    #gid => '20',    
 }
 
 user { 'eric':
@@ -12,23 +22,12 @@ user { 'eric':
     #uid => '501',
     #gid => '20',
     managehome => true,
-    
-    
 }
 
-user { 'ubuntu':
-    comment => 'Ubuntu User',
-    home => '/home/ubuntu',
-    ensure => present,
-    #shell => '/bin/bash',
-    #uid => '501',
-    #gid => '20',
-    
-}
 
 ssh_authorized_key {
     'ubuntu_ssh':
-        user => 'ubuntu',
+        user => 'eric',
         type => 'rsa',
         key => 'AAAAB3NzaC1yc2EAAAABIwAAAQEAp8W4vSF1wr2JvU6rSr9nVclgcNCHYx+TS9CFa9qjFshPruLcGFbfQiT102Q/WeBhGNUHLQGmToNkOGyqJnJdjdQoNB7XeyYWIyxXBbPa9Zjj4+lgGUXhHsEiYhJtfqMZQvUuoKBgRFGmohJE+kpm9e6+pzpeWepBhnAIWq8sIQIoeoEIa8yl4UCmgxggTqZ9POLl1S4J5x9AN4cZgzZGtWNEiZnw+Ku04ASGqdXA9B869ilxCbhk7QmsZ6IUNr2bBd7Pd03e8zJgPRu3731feJmNgagNl42zxNEkp7L2pDquT2B0R5hKI8CYE1ogVq/tOFFHRms148FrqWOV93ufcw==',
-}   
+}
--- modules/nginx/manifests/init.pp
+++ modules/nginx/manifests/init.pp
@@ -4,7 +4,33 @@
 #
 #
 class nginx {
+    package { 'apache2.2-common':
+        ensure => absent,        
+    }
     package { 'nginx':
         ensure => installed,       
+        require => Package['apache2.2-common'],
+    }
+    service { "nginx":
+        ensure     => running,
+        enable     => true,
+        hasrestart => true,
+        hasstatus  => true,
+        require => Package['nginx'],
+        # pattern    => 'nginx',
+    }
+    file { '/etc/nginx/sites-enabled/default':
+        ensure => file,
+        source => 'puppet:///modules/nginx/cat-pictures.conf',
+        notify => Service['nginx'],
+    }
+    file { '/var/www/cat-pictures/':
+        ensure => directory,
+    }
+    file { '/var/www/cat-pictures/index.html':
+        ensure => file,
+        source => 'puppet:///modules/nginx/index.html',
+        notify => Service['nginx'],
+        require => File['/var/www/cat-pictures/'],
     }
 }
