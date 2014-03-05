4f0e8b89e0ecf0b163257eee0267a4d06f5f408e
Eric Williams <eric@subcritical.org>
2014-03-05 16:29:39 +0000

    added cron

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -9,3 +9,33 @@ node 'puppet-standalone' {
     }
     
 }
+
+
+node "lb-compute-nodes" {
+  include nginx
+  include cron
+  
+  package { 'git':
+    ensure => installed,
+  }
+  package { 'joe':
+    ensure => installed,
+    
+  }
+  package { 'name':
+    ensure => installed,
+  }
+  
+  cron::job {
+      'puppet-apply':
+      minute => '*',
+      hour => '*',
+      date => '*',
+      month => '*',
+      weekday => '*',
+      user => 'root',
+      command => 'puppet apply /opt/puppet-django-demo/manifests/sites.pp --modulepath=/opt/puppet-django-demo/modules',
+      
+  }
+  
+}
